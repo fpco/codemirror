950adb99e67789b94d57b583f616509cb624b225
Michael Sloan <mgsloan@gmail.com>
2013-08-23 08:15:24 -0700

    Allow gutter folds that are only 1 line

--- addon/fold/foldgutter.js
+++ addon/fold/foldgutter.js
@@ -64,7 +64,7 @@
       } else {
         var pos = Pos(cur, 0), func = opts.rangeFinder || CodeMirror.fold.auto;
         var range = func && func(cm, pos);
-        if (range && range.from.line + 1 < range.to.line)
+        if (range && range.from.line < range.to.line)
           mark = marker(opts.indicatorOpen);
       }
       cm.setGutterMarker(line, opts.gutter, mark);
