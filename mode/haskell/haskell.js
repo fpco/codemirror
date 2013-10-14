8b218fcbfb044f8c9db452a2e87456acc72062d2
Michael Sloan <mgsloan@gmail.com>
2013-10-13 22:51:59 -0700

    Compute haskell indentation, for auto-indent and display

--- mode/haskell/haskell.js
+++ mode/haskell/haskell.js
@@ -9,9 +9,9 @@ if( Object.create === undefined ) {
 
 CodeMirror.defineMode("haskell", function(config) {
 
-  function switchState(source, setState, f) {
-    setState(f);
-    return f(source, setState);
+  function switchState(source, state, setState, f) {
+    setState(f, state);
+    return f(source, state, setState);
   }
 
   // These should all be Unicode extended, as per the Haskell 2010 report
@@ -26,39 +26,111 @@ CodeMirror.defineMode("haskell", function(config) {
   var specialRE = /[(),;[\]`{}]/;
   var whiteCharRE = /[ \t\v\f]/; // newlines are handled in tokenizer
   var layoutKeyword = /let|where|of|do/;
-  // I would include -> for case statements, but it's also type syntax.
-  var eolIndentOpen = /=|$|<-/;
 
-  function normal(source, setState) {
+
+  function block(column, indentation) {
+    return function(source, state, setState) {
+      var column = source.column() + CodeMirror.countColumn(source.current(), null, source.tabSize);
+      // If we're at the beginning of the next line after a block opener, then use normal mode.
+      if (column == 0) {
+        return switchState(source, state, setState, normal);
+      // Otherwise, the position after eating whitespace determines the indent.
+      // (f = do     bar), the start of bar is the start of the block.
+      //TODO: handle comments etc.
+      } else {
+        source.eatWhile(whiteCharRE)
+        if (!source.eol()) {
+          // Remove the old indent.
+          state.scopes.shift();
+          // NOTE: need to recompute column because it might have changed due to eating whitespace.
+          column = source.column() + CodeMirror.countColumn(source.current(), null, source.tabSize);
+          setState(normal, state, { offset: column, isBlock: true });
+        }
+        return null;
+      }
+    };
+  }
+
+  // Returns null if the indent shouldn't be recorded.
+  //
+  // FIXME: For some reason selections in the middle of indents cause the nodes
+  // to get split..
+  function indentTag(column, isBlock, isEnd, isSuggestion) {
+    if (!config.showAlignments || isSuggestion) { return false; }
+    return "indent indent-" + column +
+      (column % config.indentUnit == 0 ? " ontab-indent" : " offtab-indent") +
+      (isBlock ? " block-indent" : " plain-indent") +
+      (isEnd ? " endent" : "");
+  }
+
+  function normal(source, state, setState) {
+    var column = source.column() + CodeMirror.countColumn(source.current(), null, source.tabSize);
     // Store this line's indentation if we're at the beginning.
-    if (source.start == 0) {
-      setState(normal, source.indentation());
+    if (column == 0) {
+      var indentation = source.indentation();
+      state.indentation = indentation;
+      // Remove scopes that no longer matter.
+      var topScope;
+      while (topScope = state.scopes.shift()) {
+        if (topScope.startBlock) {
+          // Found the start of a block, need to put it back on the scopes stack.
+          state.scopes.unshift({ offset: indentation, isBlock: true });
+          break;
+        } else if (indentation >= topScope.offset) {
+          // This indent should be left alone - it's at a lower / equal indent.
+          state.scopes.unshift(topScope);
+          break;
+        }
+      }
     }
-    if (source.eatWhile(whiteCharRE)) {
-      return null;
+    var matched = whiteCharRE.exec(source.peek());
+    if (matched) {
+      if (column < state.indentation) {
+        for (var i = state.scopes.length - 1; i >= 0; i--) {
+          // Eat whitespace till it reaches the next indentation.
+          var offset = state.scopes[i].offset;
+          var isBlock = state.scopes[i].isBlock;
+          var isSuggestion = state.scopes[i].isSuggestion;
+          while (column < offset) {
+            var eaten = source.eat(whiteCharRE);
+            if (!eaten) break;
+            column += CodeMirror.countColumn(eaten, null, source.tabSize);
+          }
+          // Output an "indent" span, allowing indent guides to be styled.
+          if (source.current().length > 0) {
+            if (column == offset && column != 0 && whiteCharRE.test(source.peek())) {
+              var result = indentTag(column, isBlock, false, isSuggestion);
+              if (result) return result;
+            // If we're at the end of indentation, but it's a block indent, then
+            // mark that span as an indent.  We know we're at the end because if
+            // the loop was able to eat enough whitespace, column == offset.
+            } else if (column + 1 == offset && source.current().length > 0 && isBlock) {
+              // Need to re-add the state.
+              setState(normal, state, { offset: state.indentation });
+              return indentTag(column, true, true, isSuggestion);
+            }
+          }
+        }
+        // Went past any indentation offset - record a plain indent.
+        if (source.eatWhile(whiteCharRE) || source.current().length > 0) {
+          setState(normal, state, { offset: state.indentation });
+          column = source.column() + CodeMirror.countColumn(source.current(), null, source.tabSize);
+          return indentTag(column, false, true, false)
+        }
+      }
+      if (source.eatWhile(whiteCharRE)) {
+        return null;
+      }
     }
 
     //TODO: Don't indent after module header.
-    var matched = source.match(layoutKeyword);
-    if (matched) {
-      if (source.eol()) {
-        setState(normal, source.indentation() + config.indentUnit);
-        return null;
-      } else if (whiteCharRE.test(source.peek())) {
-        setState(normal, source.column() + matched[0].length + 1);
-        return null;
+    if (source.match(layoutKeyword)) {
+      if (source.eol() || whiteCharRE.test(source.peek)) {
+        var indent = state.indentation + config.indentUnit;
+        setState(block(column, indent), state, { offset: indent, startBlock: true });
+        return "variable";
       } else {
-        source.backUp(matched[0].length);
-      }
-    }
-    // Auto-indent after some tokens.
-    matched = source.match(eolIndentOpen);
-    if (matched) {
-      if (source.eol()) {
-        setState(normal, source.indentation() + config.indentUnit);
-        return "operator";
-      } else {
-        source.backUp(matched[0].length);
+        source.backUp(source.current().length);
       }
     }
 
@@ -69,7 +141,7 @@ CodeMirror.defineMode("haskell", function(config) {
         if (source.eat('#')) {
           t = "meta";
         }
-        return switchState(source, setState, ncomment(t, 1));
+        return switchState(source, state, setState, ncomment(state, t, 1));
       }
       return null;
     }
@@ -88,7 +160,7 @@ CodeMirror.defineMode("haskell", function(config) {
     }
 
     if (ch == '"') {
-      return switchState(source, setState, stringLiteral);
+      return switchState(source, state, setState, stringLiteral);
     }
 
     if (largeRE.test(ch) && source.match(qualRE)) {
@@ -142,17 +214,24 @@ CodeMirror.defineMode("haskell", function(config) {
         t = "operator-2";
       }
       source.eatWhile(symbolRE);
+      // Auto-indent after symbols end the prior line.
+      // TODO: special case out -> in types.
+      // TODO: similar auto-indent for "if" blocks
+      // TODO: handle trailing whitespace
+      if (source.eol()) {
+        setState(normal, state, { offset: state.indentation + config.indentUnit, isSuggestion: true });
+      }
       return t;
     }
 
     return "error";
   }
 
-  function ncomment(type, nest) {
+  function ncomment(state, type, nest) {
     if (nest == 0) {
       return normal;
     }
-    return function(source, setState) {
+    return function(source, state, setState) {
       var currNest = nest;
       while (!source.eol()) {
         var ch = source.next();
@@ -162,26 +241,26 @@ CodeMirror.defineMode("haskell", function(config) {
         else if (ch == '-' && source.eat('}')) {
           --currNest;
           if (currNest == 0) {
-            setState(normal);
+            setState(normal, state);
             return type;
           }
         }
       }
-      setState(ncomment(type, currNest));
+      setState(ncomment(state, type, currNest), state);
       return type;
     };
   }
 
-  function stringLiteral(source, setState) {
+  function stringLiteral(source, state, setState) {
     while (!source.eol()) {
       var ch = source.next();
       if (ch == '"') {
-        setState(normal);
+        setState(normal, state);
         return "string";
       }
       if (ch == '\\') {
         if (source.eol() || source.eat(whiteCharRE)) {
-          setState(stringGap);
+          setState(stringGap, state);
           return "string";
         }
         if (source.eat('&')) {
@@ -191,18 +270,19 @@ CodeMirror.defineMode("haskell", function(config) {
         }
       }
     }
-    setState(normal);
+    setState(normal, state);
     return "error";
   }
 
-  function stringGap(source, setState) {
+  function stringGap(source, state, setState) {
     if (source.eat('\\')) {
-      return switchState(source, setState, stringLiteral);
+      return switchState(source, state, setState, stringLiteral);
     }
     source.next();
-    setState(normal);
+    setState(normal, state);
     return "error";
   }
+
 
   var wellKnownWords = (function() {
     var wkw = Object.create(null);
@@ -267,23 +347,43 @@ CodeMirror.defineMode("haskell", function(config) {
   })();
 
   return {
-    startState: function ()  { return { f: normal, haskellIndent: 0 }; },
-    copyState:  function (s) { return { f: s.f, haskellIndent: s.haskellIndent }; },
+    startState: function ()  { return { f: normal, scopes: [{offset: 0, type: "haskell"}] }; },
+    copyState:  function (s) { return { f: s.f, scopes: s.scopes.concat([]) }; },
 
     token: function(stream, state) {
-      var t = state.f(stream,
-        function(s, i) {
+      //TODO: fix this callback madness.
+      var t = state.f(stream, state,
+        function(s, state, indent) {
           state.f = s;
-          if (i !== undefined) {
-            state.haskellIndent = i;
+          if (indent) {
+            if (state.scopes.length == 0) {
+              state.scopes.unshift(indent);
+            } else {
+              for (var i = 0; i < state.scopes.length; i++) {
+                var topScope = state.scopes[i];
+                if (topScope.offset <= indent.offset) {
+                  var exists = topScope.offset == indent.offset;
+                  if (!(exists && topScope.isBlock)) {
+                    state.scopes.splice(i, exists ? 1 : 0, indent);
+                  }
+                  break;
+                }
+              }
+            }
           }
         });
       var w = stream.current();
       return (w in wellKnownWords) ? wellKnownWords[w] : t;
     },
 
+    //FIXME: If the user makes a newline right after a token where its
+    // indentation is sensitive to being on the eol (e.g., operators), then it
+    // won't be considered.
     indent: function(state) {
-      return state.haskellIndent;
+      for (var i = 0; i < state.scopes.length; i++) {
+        return state.scopes[i].offset;
+      }
+      return CodeMirror.Pass;
     }
   };
 
